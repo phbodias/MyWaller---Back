3dffc01f93052120ed86e363900f1ea84fece4a5
Pedro <pedrohenriquebritooliveira@gmail.com>
2022-06-30 01:26:20 -0300

    Estrutura basica do projeto

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
-app.use(express.json());
-app.use(cors());
+app.use(express.json(), cors());
 
 // Rotas para fazer cadastro/login do usuário
 app.post('/login', loginUser);
