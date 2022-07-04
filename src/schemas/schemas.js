511ddaf5c421013200a9f8da8306798704d6e13f
Pedro <pedrohenriquebritooliveira@gmail.com>
2022-07-04 12:22:25 -0300

    Adicionando rotas

--- src/schemas/schemas.js
+++ src/schemas/schemas.js
@@ -10,3 +10,9 @@ export const cadastroSchema = joi.object({
     email: joi.string().email().required(),
     password: joi.string().required()
 });
+
+export const postSchema = joi.object({
+    titulo: joi.string().required(),
+    tipo: joi.string().required(),
+    valor: joi.string().required()
+});
